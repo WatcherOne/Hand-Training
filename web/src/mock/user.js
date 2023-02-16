3724e1513dbf622da7e3deb623fe2cc4b20726ae
zhubo <286154864@qq.com>
2023-02-16 15:19:37 +0800

    add mock data for user

---
+++ web/src/mock/user.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id: 1,
+        userCode: '001',
+        userName: 'zhubo',
+        age: 25,
+        sex: 0,
+        idCard: '510131199901011234',
+        password: '123456'
+    },
+    {
+        id: 2,
+        userCode: '002',
+        userName: '测试用户A',
+        age: 20,
+        sex: 1,
+        idCard: '510131199901011237',
+        password: '12345678'
+    },
+    {
+        id: 3,
+        userCode: '003',
+        userName: '测试用户B',
+        age: 22,
+        sex: 0,
+        idCard: '510131199901011238',
+        password: '12345678'
+    }
+]
